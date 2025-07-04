eee24f9e2f614c9a6e9eb68497dd8e670ef00a64
alexismendozaa <afmendozaf@uce.edu.ec>
2025-07-03 19:33:21 -0500

    🔧 chore: update dependencies to latest versions

--- users-liked-ms/app/services/authentication_service.rb
+++ users-liked-ms/app/services/authentication_service.rb
@@ -3,7 +3,7 @@ require 'jwt'
 class AuthenticationService
   def self.decode(token)
     secret_key = ENV['JWT_SECRET']
-    decoded = JWT.decode(token, secret_key, true, { algorithm: 'HS256' })
+    decoded = JWT.decode(token, secret_key, true, { algorithm: 'HS256', verify_expiration: false })
     decoded.first['userId']  # Cambié 'user_id' por 'userId' para coincidir con el campo en el token
   rescue JWT::DecodeError
     nil
