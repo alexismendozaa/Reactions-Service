fd34b157ccf2cf9cb1f601ab7934d6c878448a3b
alexismendozaa <afmendozaf@uce.edu.ec>
2025-07-11 13:28:59 -0500

    🔧 chore: update health check routes to use a consistent response format

--- users-liked-ms/config/routes.rb
+++ users-liked-ms/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs-users-liked'
   mount Rswag::Api::Engine => '/api-docs-users-liked'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+get '/up', to: proc { [200, { 'Content-Type' => 'application/json' }, [ { status: 'ok' }.to_json ]] }, as: :rails_health_check
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
